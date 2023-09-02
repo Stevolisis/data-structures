405d24d6a660b3504b87c13c042d80c421acd256
Steven Joseph <stevolisisjoseph@gmail.com>
2023-09-02 20:07:55 +0100

    Linkedlist getSize and isEmpty

---
+++ Linked_list.js
@@ -0,0 +1,26 @@
+
+class Node{
+    constructor(value){
+        this.value= value;
+        this.next= null;
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+
+    isEmpty(){
+        return this.size === 0;
+    }
+
+    getSize(){
+        return this.size;
+    }
+}
+
+const list=new LinkedList();
+
+console.log(list.isEmpty());
