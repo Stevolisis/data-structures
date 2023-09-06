d562c3e749a3cabc843b50f89dd240a64315db9b
Steven Joseph <stevolisisjoseph@gmail.com>
2023-09-06 18:03:09 +0100

    removeFromEnd

---
+++ Linked_list_tail.js
@@ -0,0 +1,102 @@
+
+class Node{
+    constructor(value){
+        this.value= value;
+        this.next= null;
+    }
+}
+
+class LinkedListTail{
+    constructor(){
+        this.head= null;
+        this.tail= null;
+        this.size = 0;
+    }
+
+    isEmpty(){
+        return this.size === 0;
+    }
+
+    prepend(value){
+        const node= new Node(value);
+
+        if(this.isEmpty()){
+            this.head = node;
+            this.tail = node;
+        }else{
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size++
+    }
+
+    append(value){
+        const node= new Node(value);
+        
+        if(this.isEmpty()){
+            this.head = node;
+            this.tail = node;
+        }else{
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.size++
+    }
+
+    removeFromFront(){
+        if(this.isEmpty()){
+            return null;
+        }
+        const value = this.head.value;
+        this.head = this.head.next;
+        this.size--
+        return value;
+    }
+
+    removeFromEnd(){
+        if(this.isEmpty()){
+            return null;
+        }
+        const value= this.tail.value;
+        if(this.size === 1){
+            this.head = null;
+            this.tail = null;
+        }else{
+            let prev= this.head;
+            while(prev.next !== this.tail){
+                prev = prev.next;
+            }
+            prev.next = null;
+            this.tail = prev;
+        }
+        this.size--
+        return value;
+    }
+
+    print(){
+        if(this.isEmpty()){
+            console.log('List is Empty');
+        }else{
+            let curr=this.head;
+            let listValues='';
+
+            while(curr){
+                listValues += `${curr.value} `;
+                curr = curr.next;
+            }
+            console.log(listValues);
+        }
+        return
+    }
+}
+
+
+const list=new LinkedListTail();
+list.prepend(3)
+list.prepend(4)
+list.prepend(5)
+list.append(2)
+list.append(1)
+list.removeFromEnd()
+list.removeFromFront()
+list.print();
