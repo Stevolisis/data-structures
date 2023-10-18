d148a9c93db277c643b65409a9e4fb0d18fb1947
Steven Joseph <stevolisisjoseph@gmail.com>
2023-10-18 22:23:21 +0100

    BinarySearchTree class, only isEmpty() for now

---
+++ Binary_search_tree.js
@@ -0,0 +1,17 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null;
+    }
+
+    isEmpty(){
+        return this.root === null;
+    }
+}
