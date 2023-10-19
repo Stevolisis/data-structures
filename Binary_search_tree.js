6decca923fa4739705aeb6afffb7daff8200ed0f
Steven Joseph <stevolisisjoseph@gmail.com>
2023-10-19 12:32:58 +0100

    insert and insertNewNode recursion

--- Binary_search_tree.js
+++ Binary_search_tree.js
@@ -14,4 +14,29 @@ class BinarySearchTree{
     isEmpty(){
         return this.root === null;
     }
+
+    insert(value){
+        const node = new Node(value);
+        if(this.root){
+            this.insertNewNode(this.root,node);
+        }else{
+            this.root = node;
+        }
+    }
+
+    insertNewNode(root,newNode){
+        if(newNode.value < root.value){
+            if(!root.left){
+                root.left = newNode;
+            }else{
+                this.insertNewNode(root.left,newNode);
+            }
+        }else{
+            if(!root.right){
+                root.right = newNode;
+            }else{
+                this.insertNewNode(root.right,newNode);
+            }
+        }
+    }
 }
